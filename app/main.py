efbf1831a041584f9c902f5fa45baa24f5fa9409
Pedro Teixeira <pedroteixeira0@gmail.com>
2026-05-31 19:39:08 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 import sys
 
 
+def is_valid_command(cmd: str) -> bool:
+    return False
+
+
 def main():
     sys.stdout.write("$ ")
+    user_input: str = input()
+    if not is_valid_command(user_input):
+        print(f"{user_input}: command not found")
+
 
 
 if __name__ == "__main__":
