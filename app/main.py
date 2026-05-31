8f9ba2456db397e4f8495ce07cdcbec7552d26b5
Pedro Teixeira <pedroteixeira0@gmail.com>
2026-05-31 19:49:47 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,18 +2,19 @@ import sys
 
 
 _builtins: dict[str, callable] = {
-    "exit": lambda: exit(0)
+    "exit": lambda args: exit(0),
+    "echo": lambda args: print(" ".join(args[1:]) if len(args) > 1 else "")
 }
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        user_input: str = input()
-        if not user_input in _builtins:
-            print(f"{user_input}: command not found")
+        user_input: list[str] = input().split(" ")
+        if not user_input or user_input[0] not in _builtins:
+            print(f"{user_input[0]}: command not found")
         else:
-            _builtins[user_input]()
+            _builtins[user_input[0]](user_input)
 
 
 if __name__ == "__main__":
