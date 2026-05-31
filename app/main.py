42a9b3079cf1ebc1fdaca3b6a7bd68db26d4c8fd
Pedro Teixeira <pedroteixeira0@gmail.com>
2026-05-31 19:54:02 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ import sys
 
 _builtins: dict[str, callable] = {
     "exit": lambda args: exit(0),
-    "echo": lambda args: print(" ".join(args[1:]) if len(args) > 1 else "")
+    "echo": lambda args: print(" ".join(args[1:]) if len(args) > 1 else ""),
+    "type": lambda args: _builtin_type(args[1]) if len(args) > 1 else print_not_found("")
 }
+
+def _builtin_type(command: str) -> None:
+    if command in _builtins:
+        print(f"{command} is a shell builtin")
+    else:
+        print_not_found(command)
+
+def print_not_found(command: str) -> None:
+    print(f"{command}: not found")
 
 
 def main():
@@ -12,7 +22,7 @@ def main():
         sys.stdout.write("$ ")
         user_input: list[str] = input().split(" ")
         if not user_input or user_input[0] not in _builtins:
-            print(f"{user_input[0]}: command not found")
+            print_not_found(user_input[0])
         else:
             _builtins[user_input[0]](user_input)
 
