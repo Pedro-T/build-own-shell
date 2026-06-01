f9123b5f97aacfa9e3f0b09da5f0911bdebc6212
Pedro Teixeira <pedroteixeira0@gmail.com>
2026-06-01 18:50:06 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
-
+import os
+from pathlib import Path
 
 _builtins: dict[str, callable] = {
     "exit": lambda args: exit(0),
@@ -7,11 +8,25 @@ _builtins: dict[str, callable] = {
     "type": lambda args: _builtin_type(args[1]) if len(args) > 1 else print_not_found("")
 }
 
+def is_path_command(command: str) -> tuple[bool, Path|None]:
+    path_var: str = os.environ["PATH"]
+    for seq in path_var.split(os.pathsep):
+        dir_path = Path(seq)
+        for file in dir_path.iterdir():
+            if file.name == command:
+                return True, dir_path / file
+    return False, None
+
+
 def _builtin_type(command: str) -> None:
     if command in _builtins:
         print(f"{command} is a shell builtin")
-    else:
-        print_not_found(command)
+        return
+    valid, fullpath = is_path_command(command)
+    if valid:
+        print(f"{command} is {fullpath}")
+        return
+    print_not_found(command)
 
 def print_not_found(command: str) -> None:
     print(f"{command}: not found")
